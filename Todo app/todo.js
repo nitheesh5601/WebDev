81ebb96cd756e79fb0587be99386c4221d2795de
nitheesh5601 <nitheeshwaran56@gmail.com>
2024-08-26 22:35:27 +0530

    formatted the changes

--- Todo app/todo.js
+++ Todo app/todo.js
@@ -1,60 +1,64 @@
-document.getElementById("inp").addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      addTodo();
-    }
-  });
-let count=1;
-    function addTodo(){
-      //fetching input
-      const inputText = document.getElementById("inp").value.trim();
-      if(inputText === ""){
-        alert("Don't leave empty");
-        return;
+document.getElementById("inp").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTodo();
+  }
+});
+let count = 1;
+function addTodo() {
+  //fetching input
+  const inputText = document.getElementById("inp").value.trim();
+  if (inputText === "") {
+    alert("Don't leave empty");
+    return;
+  }
+  //creating new div element
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "flex");
+  newDiv.setAttribute("id", "todo-" + count);
+
+  //creating new heading element
+  var newHeading = document.createElement("li");
+  newHeading.textContent = inputText;
+
+  //creating new button element
+  const newButton = document.createElement("button");
+  newButton.textContent = "X";
+  newButton.setAttribute("onclick", "deleteTodo(" + count + ")");
+
+  //appending elements
+  newDiv.appendChild(newHeading);
+  newDiv.appendChild(newButton);
+
+  //appending to parent
+  const parentDiv = document.getElementById("mainTodolist");
+  parentDiv.appendChild(newDiv);
+
+  //increasing the count
+  count++;
+
+  //clearing the input field
+  document.getElementById("inp").value = "";
+
+  parentDiv.addEventListener(
+    "click",
+    function (e) {
+      if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
       }
-      //creating new div element
-      const newDiv = document.createElement("div");
-      newDiv.setAttribute("class", "flex");
-      newDiv.setAttribute("id", "todo-"+count);
+    },
+    false
+  );
+}
 
-      //creating new heading element
-      var newHeading = document.createElement("li");
-      newHeading.textContent = inputText; 
+function deleteTodo(index) {
+  const delText = document.getElementById("todo-" + index);
+  delText.parentNode.removeChild(delText);
+}
 
-      //creating new button element
-      const newButton = document.createElement("button");
-      newButton.textContent = "X";
-      newButton.setAttribute("onclick","deleteTodo("+count+")");
+function clearAll() {
+  const clearParentDiv = document.querySelectorAll("div[class^='flex']");
+  clearParentDiv.forEach(function (todo) {
+    todo.parentNode.removeChild(todo);
+  });
+}
 
-      //appending elements
-      newDiv.appendChild(newHeading);
-      newDiv.appendChild(newButton);
-
-      //appending to parent
-      const parentDiv = document.getElementById("mainTodolist");
-      parentDiv.appendChild(newDiv);
-
-      //increasing the count
-      count++;
-
-      //clearing the input field
-      document.getElementById("inp").value = "";
-      
-      parentDiv.addEventListener("click",function(e){
-        if(e.target.tagName === "LI"){
-            e.target.classList.toggle("checked");
-        }
-      },false)
-
-    }
-
-    function deleteTodo(index){
-      const delText = document.getElementById("todo-"+index);
-      delText.parentNode.removeChild(delText)
-    }
-
-    function clearAll(){
-      const clearParentDiv = document.querySelectorAll("div[class^='flex']")
-      clearParentDiv.forEach(function(todo){
-        todo.parentNode.removeChild(todo)
-      })
-    }
